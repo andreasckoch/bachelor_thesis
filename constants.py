220c80499061b36fb76dbda5e52d6c136454af46
Andreas Koch <ankoch@mpa-garching.mpg.de>
2018-04-25 15:09:07 +0200

    Delete constants.py

--- constants.py
+++
@@ -1,8 +0,0 @@
-data_path = "/home/andi/bachelor/data/originaldata/SGR1806_time_PCUID_energychannel.txt"
-energy_path = "/home/andi/bachelor/data/arrangeddata/energy_channels.txt"
-
-prelude_data_path = "data/SGR1806_time_PCUID_energychannel.txt"
-prelude_energy_path = "data/energy_channels.txt"
-
-ins_p = [341909/(341909+335600+329606), 335600 /
-         (341909+335600+329606), 329606/(341909+335600+329606)]
